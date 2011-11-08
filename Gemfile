a822d2cfe6fa064012afd744b7a7d9be2d82ae63
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-11-08 17:24:33 -0200

    First Commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+
+gem 'devise', '1.4.9'
+
+gem 'pg', '0.11.0'
+
+gem 'haml', '3.1.3'
+gem 'sass', '3.1.10'
+
+gem 'inherited_resources', '1.3.0'
+
+gem 'heroku'
+
+group :delopment, :test do
+  gem 'silent-postgres', '0.0.8'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers', '1.0.0.beta2'
+  gem 'machinist', '2.0.0.beta2'
+  gem 'machinist-caching'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahdoc::Application.config.secret_token = '4158d20c94e8cd81efb065b609a2335482791e80cedb8922e72f768369abbd77db1462c9794f17793395f2a8e5ee61c1402cb89152b8ebfbe544d76639c22169'
