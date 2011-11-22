192469b18b6c82263fc433431d3a5b713b533304
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-11-21 23:52:59 -0200

    relacionamento e validaçao de exclusão com table

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,12 +1,19 @@
 class Database < ActiveRecord::Base
   belongs_to :system
+  has_many :tables
 
   validates :system, :presence => true
   validates :name, :presence => true,
                    :length => {:maximum => 255, :allow_blank => true},
                    :uniqueness => {:scope => :system_id, :allow_blank => true}
 
+  before_destroy :can_be_destroyed_when_there_are_not_tables
+
   def to_s
     name.to_s
+  end
+protected
+  def can_be_destroyed_when_there_are_not_tables
+    tables.empty?
   end
 end
--- spec/models/database_spec.rb
+++ spec/models/database_spec.rb
@@ -4,6 +4,7 @@ require File.expand_path("spec/spec_helper")
 describe Database do
   context "association" do
     it {should belong_to :system}
+    it {should have_many :tables}
   end
   
   context "validations" do
@@ -14,6 +15,18 @@ describe Database do
     context "should be unique" do
       before(:each){ Database.make! }
       it {should validate_uniqueness_of(:name).scoped_to(:system_id) }
+    end
+
+    it "can be deleted when there aren't tables" do
+      subject.destroy
+      subject.destroyed?.should be_true
+    end
+
+    it "can't be deleted when there are tables" do
+      subject = stub_model Database, :tables => [stub_model(Table)]
+
+      subject.destroy
+      subject.destroyed?.should be_false
     end
   end
 
