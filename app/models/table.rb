c354d9efb4ea2a527ff164e2c3218cbb768256af
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-11-23 09:58:16 -0200

    associação de table com fields e validação de exclusão

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,12 +1,19 @@
 class Table < ActiveRecord::Base
   belongs_to :database
+  has_many :fields
 
   validates :database, :presence => true
   validates :name, :presence => true,
                    :length => {:maximum => 255, :allow_blank => true},
                    :uniqueness => {:scope => :database_id, :allow_blank => true}
 
+  before_destroy :can_be_destroyed_when_there_are_not_fields
+
   def to_s
     name
+  end
+protected
+  def can_be_destroyed_when_there_are_not_fields
+    fields.empty?
   end
 end
--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -4,6 +4,7 @@ require File.expand_path("spec/spec_helper")
 describe Table do
   context "association" do
     it {should belong_to :database}
+    it {should have_many :fields}
   end
   
   context "validations" do
@@ -14,6 +15,18 @@ describe Table do
     context "should be unique" do
       before(:each){ Table.make! }
       it {should validate_uniqueness_of(:name).scoped_to(:database_id) }
+    end
+
+    it "can be deleted when there aren't fields" do
+      subject.destroy
+      subject.destroyed?.should be_true
+    end
+
+    it "can't be deleted when there are fields" do
+      subject = stub_model Table, :fields => [stub_model(Field)]
+
+      subject.destroy
+      subject.destroyed?.should be_false
     end
   end
 
