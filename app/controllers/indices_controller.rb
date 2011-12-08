32f3d69463531685307eb26a09e07391675aec16
eliphio farias barros <eliphio@MacBook-de-eliphio-farias-barros.local>
2011-12-08 17:30:12 -0200

    atualizando a rota do up do index

--- app/controllers/indices_controller.rb
+++ app/controllers/indices_controller.rb
@@ -2,7 +2,7 @@ class IndicesController < InheritedController
   belongs_to :table
 protected
   def up_path
-    table_indices_path parent.table
+    database_tables_path parent.database
   end
 
   helper_method :up_path
